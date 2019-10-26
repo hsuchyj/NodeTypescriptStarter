7484364102afdf99b1209a4303df439f27a8f49e
cmeese456 <collin.meese@gmail.com>
2019-10-26 15:44:41 -0400

    menu schema defined

--- src/models/menuModel.ts
+++ src/models/menuModel.ts
@@ -2,11 +2,30 @@ import mongoose, { Document, Schema } from "mongoose";
 
 // tslint:disable-next-line: no-empty-interface
 export interface IMenu extends Document {
-    // TODO
+    title: string;
+    restaurant: mongoose.Schema.Types.ObjectId;
+    menuItems: [mongoose.Schema.Types.ObjectId];
 }
 
 const MenuSchema: Schema = new Schema ({
-    // TODO
+    title: {
+        type: String,
+        required: "Enter a title for your menu",
+        maxlength: [64, "Menu Title is limited to 64 characters"]
+    },
+
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true
+    },
+
+    menuItems: {
+        type: [mongoose.Schema.Types.ObjectId], // Array of menuItems
+        ref: "Restaurant",
+        required: "You must enter at least one menu item"
+    }
+
 });
 
 export default mongoose.model<IMenu>("Restaurant", MenuSchema);
