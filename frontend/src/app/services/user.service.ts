e1b821343dfd2ca0c41aa617fb4aeff3ee4b894f
cmeese456 <collin.meese@gmail.com>
2019-11-27 15:42:56 -0500

    started some work on login page

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,4 @@
+import { IUser, UserModel as User } from "../../../../backend/src/models/userModel";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
