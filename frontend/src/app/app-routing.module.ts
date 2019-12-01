396c92e70fb8b015a70d19fb6a2fdacce6cd072a
cmeese456 <collin.meese@gmail.com>
2019-12-01 17:07:14 -0500

    fixed display issues but login not working

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './views/about/about.component';
 import { ProfileComponent } from './views/profile/profile.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantResolver } from './restaurants/restaurant-resolver';
-import { LoginComponent } from './login/login.component';
+//import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'user', component: ProfileComponent},
   {path: 'restaurants', component: RestaurantsComponent, resolve: { cres: RestaurantResolver }},
-  {path: 'login', component: LoginComponent}
+  //{path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
