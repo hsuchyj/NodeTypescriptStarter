563ac23f8c850f3e9a8b2de47a08577cde034f94
ariegner <ARiegner18@gmail.com>
2019-12-03 21:48:57 -0500

    made restaurants into a grid

--- frontend/src/app/restaurants/restaurant-list/restaurant-list.component.ts
+++ frontend/src/app/restaurants/restaurant-list/restaurant-list.component.ts
@@ -12,12 +12,27 @@ import { RestaurantService } from '../restaurant.service';
 export class RestaurantListComponent implements OnInit {
 
   restaurants: Restaurant[];
+  centerColumn: Restaurant[] = [];
+  leftColumn: Restaurant[] = [];
+  rightColumn: Restaurant[] = [];
 
   constructor(private actr: ActivatedRoute, private restaurantService: RestaurantService) {
     this.actr.data
     .subscribe( res => {
       this.restaurants = res.restaurants;
       this.restaurantService.setRestaurants(res.restaurants);
+      for(let i = 0; i < this.restaurants.length; i++) {
+        let j = i % 3;
+        if(j === 0) {
+          this.leftColumn.push(this.restaurants[i]);
+        }
+        else if(j === 1) {
+          this.centerColumn.push(this.restaurants[i]);
+        }
+        else if(j === 2) {
+          this.rightColumn.push(this.restaurants[i]);
+        }
+      }
     });
   }
 
