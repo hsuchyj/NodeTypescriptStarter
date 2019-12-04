0682cf4a57d01db89db5ec7e1b728ab210ce2661
Eric Drejza <edrejza@udel.edu>
2019-12-03 21:58:01 -0500

    updated packages and gitignore

--- backend/src/models/restaurantModel.ts
+++ backend/src/models/restaurantModel.ts
@@ -1,4 +1,5 @@
-import mongoose, { Document, Schema } from "mongoose";
+import { Document, Schema } from "mongoose";
+import * as mongoose from "mongoose"; 
 
 export interface IRestaurant extends Document {
   alias: string;  
